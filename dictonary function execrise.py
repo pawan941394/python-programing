ed20de0b84bbcac1aabd23e50701ea9b606f88e4
pawan941394 <63099276+pawan941394@users.noreply.github.com>
2020-07-19 17:27:11 +0530

    Add files via upload

---
+++ dictonary function execrise.py
@@ -0,0 +1,5 @@
+oxford = {"name" : "name is a bascially identity of a men" , "3":"this is bascially an inteager" , "3+5": "8", "laptop": "laptop is bascially use for programming"}
+print("welcome to my oxford dictonary")
+print("enter what you want to search")
+search = str(input())
+print(oxford[search])
---
+++ typecasting.py
@@ -0,0 +1,6 @@
+# typecasting is normaly change the variable into other variables for examples i am changing a string into inteager then this is called typecating #
+a = "123"
+b = 32
+#now if we plus a + b then it show an error because a is not an inteager so we have to change now string into inteager and this is called typecastin#
+
+print(int(a) + b)
